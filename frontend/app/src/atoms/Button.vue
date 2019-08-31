638c21f83359e0b095552b059daa420a73b28eb4
kurakura0916 <40450990+kurakura0916@users.noreply.github.com>
2019-08-31 18:00:56 +0900

    Add files via upload

---
+++ frontend/app/src/atoms/Button.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-btn
+            large
+            :color="color"
+            :disabled="disabled"
+            @click="handleClick"
+    >
+        <slot />
+    </v-btn>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+
+    props: {
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        color: {
+            type: String
+        }
+    },
+
+    methods: {
+        handleClick (ev) {
+            this.$emit('click', ev)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/app/src/atoms/Card.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <v-container
+                fluid
+                grid-list-md
+                pa-2
+        >
+            <v-layout wrap>
+                <v-flex
+                        :cards="cards"
+                        v-for="card in cards"
+                        :key="card.title"
+                >
+                    <v-card>
+                        <v-card-title v-text="card.title"></v-card-title>
+                        <v-divider class="mx-4"></v-divider>
+                        <v-card-text>
+                            <v-layout align-center>
+                                <v-flex xs6 display-3 style="text-align: right">
+                                    {{card.num}}
+                                </v-flex>
+                                <v-flex xs6 style="text-align: left">
+                                    <p v-if="card.title === '予約件数'">件</p>
+                                    <p v-if="card.title ==='予約人数'">人</p>
+                                </v-flex>
+                            </v-layout>
+                        </v-card-text>
+                    </v-card>
+
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Card",
+
+    props: {
+        cards: {
+            type: Array,
+            default: null,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
